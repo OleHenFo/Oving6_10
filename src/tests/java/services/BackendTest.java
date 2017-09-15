8e4a9182d43d9a21e9d5de82912f3636e78901eb
olehenfo <olehenrik3@gmail.com>
2017-09-15 09:04:55 +0200

    Added test

---
+++ src/tests/java/services/BackendTest.java
@@ -0,0 +1,21 @@
+package services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * Created by olehe on 15-Sep-17.
+ *
+ */
+class BackendTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+}
